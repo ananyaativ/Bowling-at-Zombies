a79ec4fb2da08fb7f710e3f4a970a2858a8c038d
Shah <arnavss2@illinois.edu>
2023-11-03 17:25:07 -0500

    Added chase script for zombies

---
+++ Assets/Chase.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chase : MonoBehaviour
+{
+    float rotationSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = Camera.main.transform.position - this.transform.position;
+        direction.y = 0;
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Time.deltaTime);
+    }
+}
