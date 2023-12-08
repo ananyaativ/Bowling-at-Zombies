9764b50fb806536fdc1b70a5406da236d2cae4c8
ananyaativ <84693150+ananyaativ@users.noreply.github.com>
2023-12-07 19:00:46 -0600

    Added new features

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -10,8 +10,6 @@ public class Zombie : MonoBehaviour
 
     Vector3[] boundary;
 
-    private int spawnMultiplier = 10;
-
     // Start is called before the first frame update
     void Start()
     {
